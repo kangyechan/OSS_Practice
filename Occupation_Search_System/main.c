e6143d240a08e83c9379c46ce9c945296a04d055
HAMANG <43426573+kangyechan@users.noreply.github.com>
2019-05-31 20:58:08 +0900

    Update main.c

--- Occupation_Search_System/main.c
+++ Occupation_Search_System/main.c
@@ -5,35 +5,35 @@ int main(int argc, char **argv) {
     char *fileName;
     char *doc;
     int bigcnt = 0; //total count including objects in value.
-    int selectMenu=0;
+    int selectMenu = 0;
     int companyCount=0;
     // Company *companies= malloc(sizeof(Company)*100);
 
     JSON json = {0};
-    Company companies[100];
+    Company companies[10];
 
-            fileName = "OSS.json";
-            doc = readfile(fileName, &filesize); // 파일 읽음
-            fileCheck(fileName, doc); // 파일 내용 없으면 에러
-            json_parse(doc, filesize, &json, &bigcnt); // 파서 시작
-            printResult(&json, bigcnt); // 파서 출력
-            makeCompanyTokens(&json, companies, bigcnt, &companyCount);// 파서결과를 Company Array에 저장
-            printCompanies(companies, companyCount);
-            freeJson(&json, bigcnt); // 토큰 free
+    fileName = "OSS.json";
+    doc = readfile(fileName, &filesize); // 파일 읽음
+    fileCheck(fileName, doc); // 파일 내용 없으면 에러
+    json_parse(doc, filesize, &json, &bigcnt); // 파서 시작
+    printResult(&json, bigcnt); // 파서 출력
+    makeCompanyTokens(&json, companies, bigcnt, &companyCount);// 파서결과를 Company Array에 저장
+    freeJson(&json, bigcnt); // 토큰 free
 
-    while(selectMenu < 9) {
+    while(selectMenu < 8) {
         printf("\nHello!! Welcome to the world of Occupation Search System.\n");
         printf("Select one of the menus below :)\n");
         printf("************* Occupation Search System Application *************\n");
         printf("*                                                              *\n");
-        printf("*    1.              Search by company name.                   *\n");
-        printf("*    2.              Search by company size.                   *\n");
-        printf("*    3.            Search by an annual income.                 *\n");
-        printf("*    4.                Search by job type.                     *\n");
-        printf("*    5.               Enter my Information.                    *\n");
-        printf("*    6.               Recommended company.                     *\n");
-        printf("*    7.                 I wont't get a job.                    *\n");
-        printf("*    8.                   Quit Program                         *\n");
+        printf("*    1.                 Show all company.                      *\n");
+        printf("*    2.              Search by company name.                   *\n");
+        printf("*    3.              Search by company size.                   *\n");
+        printf("*    4.            Search by an annual income.                 *\n");
+        printf("*    5.                Search by job type.                     *\n");
+        printf("*    6.               Enter my Information.                    *\n");
+        printf("*    7.               Recommended company.                     *\n");
+        printf("*    8.                 I wont't get a job.                    *\n");
+        printf("*    9.                   Quit Program                         *\n");
         printf("*                                                              *\n");
         printf("****************************************************************\n");
         printf("your select : ");
@@ -41,24 +41,30 @@ int main(int argc, char **argv) {
 
         switch (selectMenu) {
         case 1:
-            searchByName(companies, companyCount);
+            printCompanies(companies, companyCount);
             break;
         case 2:
-            searchBySize(companies, companyCount);
+            searchByName(companies, companyCount);
             break;
         case 3:
-            searchBySalary(companies, companyCount);
+            searchBySize(companies, companyCount);
             break;
         case 4:
+            searchBySalary(companies, companyCount);
             break;
         case 5:
+
             break;
         case 6:
+
             break;
         case 7:
-         printf("OK. Bye! :)\n");
+
             break;
         case 8:
+            printf("OK. Bye! :)\n");
+            break;
+        case 9:
             return 0;
         default:
             break;
